cc1f1c55fd19d5a16d04db46e0a733cb9f316a5a
TanveerTahir <55933240+TanveerTahir@users.noreply.github.com>
2025-03-12 22:49:48 +0500

    A simple api using FastAPI

---
+++ README.md
@@ -0,0 +1,33 @@
+# ***Build simple APIs using FastAPI:***
+https://fastapi.tiangolo.com/
+
+# For uv installation please visit:
+https://docs.astral.sh/uv/getting-started/installation/
+
+
+# API: application programming Interface
+It is the way to connect/communicate two different services on the Internet
+
+
+# how it works:
+database + web server + API + Internet + web application
+
+
+# creating folder command in cli:
+uv init simple-api
+
+# installing module command:
+uv add fastapi[standard] # standard will add all neccessory module include with fastapi
+
+# command for activate envoirnment:
+.venv\Scripts\activate
+
+# after initializing whole project to check your code how it works run this command in your cli
+fastapi dev main.py # get swagger ui link
+
+# Paste the following link into your browser:
+http://127.0.0.1:8000/side_hustles
+http://127.0.0.1:8000/money_quotes
+
+# swagger UI:
+https://127.0.0.1:8000/docs
---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+# we will build two simple endpoints
+# side hustles
+# money quotes
+
+side_hustles = [
+    "Freelancing - start offering your skills on platforms like Upwork, Fiverr, and Freelancer",
+    "Dropshipping - start an online store without having to hold inventory",
+    "Stocl Market - start investing in the stock market",
+    "Affiliate Marketing - start promoting products and earn a commission for every sale you make",
+    "Crypto Trading - start trading cryptocurrencies",
+    "Online Courses - start creating and selling online courses",
+    "Print on Demand - start selling custom products with your designs",
+    "Blogging - start a blog and monetize it with ads, sponsored posts, and affiliate marketing",
+    "YouTube Channel - start a YouTube channel and monetize it with ads and sponsored content",
+    "Social Media Management - start managing social media accounts for businesses",
+    "App Development - start developing mobile apps for clients",
+    "Web Development - start building websites for clients",
+    "Graphic Design - start offering graphic design services",
+    "Virtual Assistant - start offering virtual assistant services",
+    "Ebook Writing - start writing and selling ebooks",
+    "Podcasting - start a podcast and monetize it with ads and sponsorships",
+    "Online Tutoring - start offering online tutoring services",
+    "SEO Consulting - start offering SEO consulting services",
+    "Copywriting - start offering copywriting services",
+    "Photography - start selling your photos online",
+    "Ecommerce Store - start an online store selling physical products",
+    "Online Coaching - start offering online coaching services",
+    "Webinar Hosting - start hosting webinars and monetize them with ads and sponsorships",
+    "Membership Site - start a membership site and charge a monthly fee for access to premium content",
+    "Online Store - start an online store selling physical products",
+    "Online Courses - start creating and selling online courses",
+]
+
+
+money_quotes = [
+    "The best way to predict the future is to create it.",
+    "Don't let the fear of losing be greater than the excitement of winning",
+    "The only limit to our realization of tomorrow will be our doubts of today.",
+    "The best investment you can make is in yourself.",
+    "The only way to do great work is to love what you do.",
+    "The future belongs to those who believe in the beauty of their dreams.",
+    
+]
+
+@app.get("/")
+def read_root():
+    return{
+        "message" : "Hello World, Go to /side_hustles or /money_quotes to get a random side hustle or money quote."
+    }
+
+@app.get("/side_hustles")
+def get_side_hustles():
+    """Returns a random side hustle idea"""
+    return{"side_hustle": random.choice(side_hustles)}
+
+@app.get("/money_quotes")
+def get_money_quotes():
+    """Returns a random money quote"""
+    return{"money_quote": random.choice(money_quotes)}
+
+
+
+
